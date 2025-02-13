dddd5cc8c263553f74f4beb2007bb01a89617eee
Alexdudley95 <alexander.dudley95@gmail.com>
2025-02-13 19:51:35 +0000

    updated placeholder textures - added area for UI - changed <a> to <g> temp fix while working on ui

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ for(let i = 0; i < test.length; i++){
   var inner = test[i];
   $('#game-area').append("<div id=" + i + ">");
     for(let j = 0; j < inner.length; j++){
-      $('#' + i).append("<a id='"+ i +"-"+ j +"'>");
+      $('#' + i).append("<g id='"+ i +"-"+ j +"'>");
       $("#" + i +"-"+ j).append("<img src='img/dirt.png' width='64' height='64' class='dirt'></a>");
     }
  $('#game-area').append("</div")
 }
 
-$('a').click(function(){
+$('g').click(function(){
   console.log($(this).html())
   if($(this).html() == '<img src="img/plowed.png" width="64" height="64" class="plowed">' ){
     $(this).html("<img src='img/dirt.png' width='64' height='64' class='dirt'>")
