d24b387d41623cf85db939ca528d2fb14bd4c8af
Alexdudley95 <alexander.dudley95@gmail.com>
2025-02-16 18:45:06 +0000

    added vars for html tiles, now being used for get/set. Added water + sprouts

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const test =[
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 3, 0, 0],
   [0, 0, 0, 0, 0, 0]
   ];
 
@@ -9,7 +11,10 @@ var flag = false;
 
 var money = 0;
 
-$(('#money-text')).text("Money: " + money);
+var dirt  = '<img src="img/dirt.png" width="64" height="64" class="dirt">';
+var plowed  = '<img src="img/plowed.png" width="64" height="64" class="plowed">';
+var sprout  ='<img src="img/sprout.png" width="64" height="64" class="sprout">';
+var water   = '<img src="img/water.png" width="64" height="64" class="water">';
 
 console.log(test.length);
 for(let i = 0; i < test.length; i++){
@@ -17,16 +22,31 @@ for(let i = 0; i < test.length; i++){
   $('#game-area').append("<div id=" + i + ">");
     for(let j = 0; j < inner.length; j++){
       $('#' + i).append("<g id='"+ i +"-"+ j +"'>");
-      $("#" + i +"-"+ j).append("<img src='img/dirt.png' width='64' height='64' class='dirt'></a>");
+      $("#" + i +"-"+ j).append( dirt + "</g>");
     }
  $('#game-area').append("</div")
 }
 
 function shovelClick(input){
-  if($(input).html() == '<img src="img/plowed.png" width="64" height="64" class="plowed">' ){
-    $(input).html("<img src='img/dirt.png' width='64' height='64' class='dirt'>")
-  }else{
-    $(input).html("<img src='img/plowed.png' width='64' height='64' class='plowed'>")
+  console.log($(input).html());
+  if($(input).html() == dirt){
+    test[input] = 1;
+    updateSprites(input);
+  }else if($(input).html() == plowed){
+    test[input] = 0;
+    updateSprites(input);
+    console.log(test[input]);
+  }
+}
+
+function handClick(input){
+  console.log($(input).html());
+  if($(input).html() == plowed ){
+    test[input] = 2;
+    updateSprites(input);
+  }else if ($(input).html() == sprout ){
+    test[input] = 3;
+    updateSprites(input);
   }
 }
 
@@ -41,10 +61,9 @@ $('#ui-button-hand').click(function(){
 })
 
 $('g').click(function(){
-
   switch(toolChoice){
     case 0:
-      console.log("Hand!")
+      handClick($(this));
       break;
     case 1:
       shovelClick($(this));
@@ -53,4 +72,18 @@ $('g').click(function(){
 
 })
 
+function updateSprites(input){
+  console.log(test[input]);
+  if(test[input] == 0){
+    $(input).html(dirt);
+  }else if (test[input] == 1){
+    $(input).html(plowed);
+  }else if (test[input] == 2){
+    $(input).html(sprout);
+  }else if(test[input] == 3){
+    $(input).html(water);
+  }
+}
+
+$(('#money-text')).text("Money: " + money +"  |  " + toolChoice);
 
