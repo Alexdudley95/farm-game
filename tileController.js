f1be1f57707d72a651747ed16b0056dbd01024e4
Alexdudley95 <alexander.dudley95@gmail.com>
2025-03-07 11:23:20 +0000

    - add all checks to water check

--- tileController.js
+++ tileController.js
@@ -34,30 +34,74 @@ class tileController {
 
   checkSurroundingTiles(tileArray, check, depth){
     let comp = 1;
-      for(let i = 0; i < tileArray.length; i++){
-        let inner = tileArray[i]
-        for(let j = 0; j < inner.length; j++){
-          if(i == 0 && j == 0){
-            if((this.checkRight(tileArray, check, i, j, comp) == true) ||
+    for(let i = 0; i < tileArray.length; i++){
+      let inner = tileArray[i]
+      for(let j = 0; j < inner.length; j++){
+        //corner checks
+        if(i == 0 && j == 0){
+          if((this.checkRight(tileArray, check, i, j, comp) == true) ||
               (this.checkBelow(tileArray, check, i, j, comp) == true)){
                 tileArray[i][j].isTouchingWater(depth);
               }
-          }else if( i == 5 && j == 5){
-            if((this.checkLeft(tileArray, check, i, j, comp) == true) ||
-            (this.checkAbove(tileArray, check, i, j, comp) == true)){
-              tileArray[i][j].isTouchingWater(depth);
-            }
-          }else if(j == 0){
-
-          }else if(j == 5){
-
+        }else if( i == 5 && j == 5){
+          if((this.checkLeft(tileArray, check, i, j, comp) == true) ||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        }else if(i == 5 && j == 0){
+          if((this.checkRight(tileArray, check, i, j, comp) == true) ||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        }else if(i == 0 && j == 5){
+          if((this.checkLeft(tileArray, check, i, j, comp) == true) ||
+          (this.checkBelow(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        //top & bottom row checks
+        }else if(i == 0){
+          if((this.checkRight(tileArray, check, i, j, comp) == true) ||
+          (this.checkBelow(tileArray, check, i, j, comp) == true)   ||
+          (this.checkLeft(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        }else if(i == 5){
+          if((this.checkRight(tileArray, check, i, j, comp) == true)||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)   ||
+          (this.checkLeft(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        //Left & right col checks
+        }else if(j == 0){
+          if((this.checkRight(tileArray, check, i, j, comp) == true)||
+          (this.checkBelow(tileArray, check, i, j, comp) == true)   ||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        }else if(j == 5){
+          if((this.checkLeft(tileArray, check, i, j, comp) == true) ||
+          (this.checkBelow(tileArray, check, i, j, comp) == true)   ||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
+          }
+        }else{
+          if((this.checkLeft(tileArray, check, i, j, comp) == true) ||
+          (this.checkBelow(tileArray, check, i, j, comp) == true)   ||
+          (this.checkAbove(tileArray, check, i, j, comp) == true)   ||
+          (this.checkRight(tileArray, check, i, j, comp) == true)){
+            tileArray[i][j].isTouchingWater(depth);
           }
         }
-      }
 
+
+
+      }
+    }
   }
 
-
 }
+
+
+
 
 export default tileController
